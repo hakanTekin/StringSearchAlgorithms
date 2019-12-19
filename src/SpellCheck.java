6651877e4abadf869594e49a7e600f41da7b80f7
Hakan Ahmet Tekin <hahmet.tekin@tedu.edu.tr>
2019-12-19 15:55:57 +0300

    Added Dependencies and created basic structure of the project

--- src/SpellCheck.java
+++ src/SpellCheck.java
@@ -1,7 +1,17 @@
+import java.io.File;
+
+import Dependencies.Stopwatch;
 
 public class SpellCheck {
 
 	public static void main(String[] args) {
 		
+	}
+	
+	File dictionary, text;
+	
+	public SpellCheck(String dictionaryName, String textName){
+		this.dictionary = new File(dictionaryName);
+		this.text = new File(textName);
 	}
 }
