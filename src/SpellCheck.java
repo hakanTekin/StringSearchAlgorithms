403aacc74eadc87dba10f9f3f728c6b7bdf2ea77
Hakan Tekın <ahmethakantekin14@gmail.com>
2019-12-21 20:24:11 +0300

    SpellCheck gives the same results as example shown in assignment pdf. But that may not be sufficent

--- src/SpellCheck.java
+++ src/SpellCheck.java
@@ -3,6 +3,8 @@ import java.io.FileNotFoundException;
 import java.util.ArrayList;
 import java.util.Scanner;
 
+import Dependencies.BoyerMoore;
+import Dependencies.KMP;
 import Dependencies.Stopwatch;
 import Dependencies.TST;
 import Dependencies.TrieST;
@@ -13,28 +15,31 @@ public class SpellCheck {
 		Scanner sc = new Scanner(System.in);
 		System.out.println("Please enter the two file names");
 		System.out.print("Dictionary Filename : ");
-		String dictionaryFileName = sc.next();
+		String dictionaryFileName = "dictionary.txt";//sc.next();
 		
-		System.out.print("Text Filename : ");
-		String textFileName = sc.next();
-		
+		System.out.print("\nText Filename : ");
+		String textFileName = "testWrongWords.txt";//sc.next();
+		System.out.println();
+
 		SpellCheck spellCheck = new SpellCheck(dictionaryFileName, textFileName);
 	}
-	
+
 	File dictionary, text;
 	TrieST<Integer> dictionaryTrie;
-	
+
 	public SpellCheck(String dictionaryName, String textName){
 		this.dictionary = new File(dictionaryName);
 		this.text = new File(textName);
+		createTrie();
+		searchText();
 	}
-	
+
 	private TrieST<Integer> createTrie() {
 		TrieST<Integer> trie = new TrieST<Integer>();
 		Scanner sc;
 		try {
 			sc = new Scanner(this.dictionary);
-			for(int i =0; !sc.hasNext(); i++) {
+			for(int i =0; sc.hasNext(); i++) {
 				trie.put(sc.next(), i);
 			}
 			dictionaryTrie = trie;
@@ -45,7 +50,7 @@ public class SpellCheck {
 			return null;
 		}
 	}
-	
+
 	private TST<Integer> createTST() {
 		TST<Integer> trie = new TST<Integer>();
 		Scanner sc;
@@ -60,34 +65,38 @@ public class SpellCheck {
 			return null;
 		}
 	}
-	
+
 	//Search the text one word at a time. If the word is misspelled, suggest the most similar word(s).
 	private void searchText() {
 		try {
 			Scanner sc = new Scanner(text);
 			String curWord;
 			int indexCounter = 0;
-			String suggestion;
-			
+
 			while(sc.hasNext()) {
 				curWord = sc.next();
-				if(!this.dictionaryTrie.contains(curWord)){ //if true, then the word is misspelled
+				if(!this.dictionaryTrie.contains(curWord.toUpperCase())){ //if true, then the word is misspelled
 					//SuggestWord can be switched with Suggest3Word. Which suggests 3 words, instead of 1
-					suggestion = suggestWord(curWord);
-					
-					System.out.println("Error: " + curWord + "(index:" + indexCounter + " )- suggested: " + suggestion);
+					System.out.println("Error: " + curWord + "(index:" + indexCounter + ")- suggested: " + suggestWord(curWord));
 				}
 				indexCounter++;
 			}
 		} catch (FileNotFoundException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
-			
+
 		}
 	}
-	
+
 	private String suggestWord(String wrongWord) {
 		String suggestion = "";
+		for(int i = wrongWord.length()-1; i >= 0; i--) {
+			String ww = wrongWord.substring(0,i).toUpperCase();
+			if(dictionaryTrie.contains(ww)) {
+				suggestion = ww;
+				break;
+			}
+		}
 		return suggestion;
 	}
 	private String[] suggest3Word(String wrongWord) {
