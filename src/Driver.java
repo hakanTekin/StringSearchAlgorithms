83a1c35cf9f1adaf97a4d0c800645e977058d46a
AkdenizKutayOcal <akutay.ocal@tedu.edu.tr>
2019-12-22 19:22:55 +0300

    update on part2

--- src/Driver.java
+++ src/Driver.java
@@ -7,9 +7,13 @@ public class Driver {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
-		String pattern = "It is a far, far better thing that I do, than I have ever done";
-		String inputFile = "tale.txt";
-		
+		//String pattern = "It is a far, far better thing that I do, than I have ever done";
+		//String inputFile = "tale.txt";
+		//String pattern = "I say things happen not as we plan";
+		//String inputFile = "war+peace.txt";
+		String pattern = "Torrid Zone";
+		String inputFile = "dickens.txt";
+		//String pattern = "which we are not conscious";
 		Experiments experiment = new Experiments(pattern, inputFile);
 	}
 
