130729e880327e27c87d35abc3376ebc5a1c1255
Hakan Tekın <ahmethakantekin14@gmail.com>
2019-12-22 22:03:26 +0300

    Added comments to spellCheck

--- src/SpellCheck.java
+++ src/SpellCheck.java
@@ -11,7 +11,9 @@ import Dependencies.TrieST;
 
 public class SpellCheck {
 
+	//Main function, runs only this program
 	public static void main(String[] args) {
+		
 		Scanner sc = new Scanner(System.in);
 		System.out.println("Please enter the two file names");
 		System.out.print("Dictionary Filename : ");
@@ -21,6 +23,7 @@ public class SpellCheck {
 		String textFileName = "long.txt";//sc.next();
 		System.out.println();
 
+		//SpellCheck object created. Constructor does everything to suggest words automatically.
 		SpellCheck spellCheck = new SpellCheck(dictionaryFileName, textFileName);
 	}
 
@@ -30,17 +33,20 @@ public class SpellCheck {
 	public SpellCheck(String dictionaryName, String textName){
 		this.dictionary = new File(dictionaryName);
 		this.text = new File(textName);
-		createTrie();
+		createTrie(); //Tree creation from dictionary file
 		Stopwatch sw = new Stopwatch();
 		searchText();
 		System.out.println("Elapsed Time : " + sw.elapsedTime());
 	}
 
+	//Creates a Trie from given file dictionary
 	private TrieST<Integer> createTrie() {
 		TrieST<Integer> trie = new TrieST<Integer>();
 		Scanner sc;
 		try {
+			
 			sc = new Scanner(this.dictionary);
+			//For each word in the dictionary. Put lowercase versions to the trie
 			for(int i =0; sc.hasNext(); i++) {
 				trie.put(sc.next().toLowerCase(), i);
 			}
@@ -53,6 +59,7 @@ public class SpellCheck {
 		}
 	}
 
+	//Creates a Ternary Trie.
 	private TST<Integer> createTST() {
 		TST<Integer> trie = new TST<Integer>();
 		Scanner sc;
@@ -73,11 +80,14 @@ public class SpellCheck {
 		try {
 			Scanner sc = new Scanner(text);
 			String curWord;
-			int indexCounter = 0;
+			int indexCounter = 0; //Counter to know which word the program checks.
 
+			//For each word in the text
 			while(sc.hasNext()) {
 				curWord = sc.next();
-				if(!this.dictionaryTrie.contains(curWord.toLowerCase())){ //if true, then the word is misspelled
+				if(!this.dictionaryTrie.contains(curWord.toLowerCase())){ //if the statement is true, then the word is misspelled
+					
+					//Print out a message for misspelled word and suggest a new word.
 					//SuggestWord can be switched with Suggest3Word. Which suggests 3 words, instead of 1
 					System.out.println("Error: " + curWord + "(index:" + indexCounter + ")- suggested: " + suggestWord(curWord));
 				}
@@ -90,19 +100,25 @@ public class SpellCheck {
 		}
 	}
 
+	//Suggest function. Starts from the last letter of the word and goes to the start. If a word exists in the dictionary, suggests that word.
 	private String suggestWord(String wrongWord) {
+		//Initalization
 		String suggestion = "";
-		//for(int i = 0; i<=wrongWord.length()-1; i++) {
-		for(int i = wrongWord.length(); i>0; i--) {	
+		
+		//for(int i = 0; i<=wrongWord.length()-1; i++) { ///For starting from beginning instead of last letter.
+		for(int i = wrongWord.length(); i>0; i--) {
+			//Get substring from start to i. Lowercase is taken because dictionary is in lowercase
 			String ww = wrongWord.substring(0,i).toLowerCase();
-			if(dictionaryTrie.contains(ww)) {
+			if(dictionaryTrie.contains(ww)) { //If there is a word in the dictionary that is same with the substring. Then suggestion is found.
 				suggestion = ww;
+				//Suggestion found, no need to go further.
 				break;
 			}
 		}
 		return suggestion;
 	}
 	
+	//Suggests 3 words instead of one
 	private String[] suggest3Word(String wrongWord) {
 		String[] suggestion = new String[3];
 		int count = 0;
